046f2bd343e7882d69c93a7a94b1b30c93872871
Jose Carlos  García Chillón <jcgarchi@MacBook-Pro-de-Jose.local>
2018-04-22 20:32:43 +0200

    practica realizada

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from cms import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    
+    url(r'^$', 'cms.views.barra'),
+    url(r'^pages/(\d+)$', 'cms.views.pages'),
     url(r'^admin/', include(admin.site.urls)),
 )
